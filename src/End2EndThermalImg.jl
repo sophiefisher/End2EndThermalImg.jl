6470cc2292a48810f897b5914ff6e693e7a6554e
Sophie <fisher.soph@gmail.com>
2024-11-20 09:55:00 -0500

    remove second include

--- src/End2EndThermalImg.jl
+++ src/End2EndThermalImg.jl
@@ -1,10 +1,7 @@
 module End2EndThermalImg
-
     include("prepare.jl")
     include("forward.jl")
     include("backward.jl")
-    include("prepare.jl")
     include("optimize.jl")
     include("process.jl")
-
 end
