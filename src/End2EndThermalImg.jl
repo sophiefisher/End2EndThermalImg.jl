55be77e8a54d5a1517bc8ad1c07525d5319a3c79
Sophie <fisher.soph@gmail.com>
2024-12-09 12:57:06 -0500

    add packges to module

--- src/End2EndThermalImg.jl
+++ src/End2EndThermalImg.jl
@@ -1,7 +1,23 @@
 module End2EndThermalImg
+
+    # prepare.jl
+    export PhysicsHyperParams
+
+    #surrogate.jl
+    export get_transmission
+
+    using CSV
+    using DataFrames
+    using Interpolations
+    using FastChebInterp
+    using Parameters
+    using PythonCall
+
     include("prepare.jl")
     include("forward.jl")
     include("backward.jl")
     include("optimize.jl")
     include("process.jl")
+    include("surrogate.jl")
+
 end
