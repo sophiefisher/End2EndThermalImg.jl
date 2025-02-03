d29c9ec795c2715c208e7ef377f31350235364ee
Sophie <fisher.soph@gmail.com>
2025-02-03 18:13:17 -0500

    change anaconda version; remove instantiation when starting julia

--- cluster_scripts/run_time_compute_surrogate_model.sh
+++ cluster_scripts/run_time_compute_surrogate_model.sh
@@ -7,9 +7,8 @@
 source /etc/profile
 
 #load anaconda
-module load anaconda/2023b
+module load anaconda/2023a
 module load julia/1.10.1  
 
 PROJECT_DIR="~/End2EndThermalImg.jl/Project.toml"
-julia --project=${PROJECT_DIR} -e 'using Pkg; Pkg.instantiate()'
 julia --project=${PROJECT_DIR} --threads 40 scripts/time_compute_surrogate_model.jl
