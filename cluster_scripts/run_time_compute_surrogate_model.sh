324e2c5ca2c22955feb7d1e51093a535d9df8490
Sophie Fisher <sefisher@mit.edu>
2025-02-04 15:33:32 -0500

    point CondaPkg to local python installation on the cluster

--- cluster_scripts/run_time_compute_surrogate_model.sh
+++ cluster_scripts/run_time_compute_surrogate_model.sh
@@ -11,4 +11,5 @@ module load anaconda/2023a
 module load julia/1.10.1  
 
 PROJECT_DIR="~/End2EndThermalImg.jl/Project.toml"
+export JULIA_CONDAPKG_BACKEND="Null"
 julia --project=${PROJECT_DIR} --threads 40 scripts/time_compute_surrogate_model.jl
