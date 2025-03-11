688edfc26638e895ebf53f7a03b6e4936bc7207d
Sophie <fisher.soph@gmail.com>
2025-03-11 14:31:11 -0400

    cluster submission script for computing surrogate model

---
+++ cluster_scripts/run_compute_surrogate_model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=compute_surrogate_model
+#SBATCH --output=logs/log-%j-%x.out
+#SBATCH -c 10
+#initialize module command
+source /etc/profile
+
+#load anaconda
+module load anaconda/2023a
+module load julia/1.10.1  
+
+PROJECT_DIR="~/End2EndThermalImg.jl/Project.toml"
+export JULIA_CONDAPKG_BACKEND="Null"
+julia --project=${PROJECT_DIR} --threads 10 scripts/compute_surrogate_model.jl
