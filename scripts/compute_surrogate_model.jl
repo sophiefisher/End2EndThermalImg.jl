41b17e6ac6c4d84f43042ccf38e47add338d50b9
Sophie <fisher.soph@gmail.com>
2025-03-24 14:40:49 -0400

    tweak script options

--- scripts/compute_surrogate_model.jl
+++ scripts/compute_surrogate_model.jl
@@ -10,12 +10,12 @@ php = PhysicsHyperParams(
     unit_cell_length_μm = 4.0,
     pillar_width_lb_μm = 1.8,
     pillar_width_ub_μm = 2.7,
-    pillar_width_order = 64,
+    pillar_width_order = 10,
     pillar_height_μm = 10.0,
     pillar_material = "Si_no_absorption",
     substrate_height_μm = 300.0,
     substrate_material = "Si_no_absorption",
-    nG = 10
+    nG = 1000
 )
 
 End2EndThermalImg.compute_and_save_surrogate_transmission_matrix(php)
