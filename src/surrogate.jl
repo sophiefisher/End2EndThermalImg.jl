3a260e19cf4013e2a37aba723a29711c1431f4ab
Sophie Fisher <sefisher@mit.edu>
2025-03-26 15:21:12 -0400

    python garbage collection

--- src/surrogate.jl
+++ src/surrogate.jl
@@ -68,6 +68,7 @@ function compute_surrogate_transmission_matrix(php::PhysicsHyperParams)
             substrate_ϵ = get_substrate_ϵ(λ_µm)
             transmission = get_transmission(freq, width, pillar_height, pillar_ϵ, unit_cell_length, substrate_ϵ, php.nG)
             transmission_matrix[i, j] = transmission
+            GC.gc()
         end
     end
     transmission_matrix
