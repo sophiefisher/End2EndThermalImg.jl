8c0f55bce5e4ca447a975ccc950233390db20a7b
Sophie <fisher.soph@gmail.com>
2025-03-26 15:18:56 -0400

    launch python from script instead of pythoncall

--- src/surrogate.jl
+++ src/surrogate.jl
@@ -60,15 +60,16 @@ function compute_surrogate_transmission_matrix(php::PhysicsHyperParams)
     width_chebpoints = get_width_chebpoints(php)
 
     transmission_matrix = Matrix{Complex{Float64}}(undef, length(freq_chebpoints), length(width_chebpoints))
-    PythonCall.GIL.@unlock Threads.@threads for j in eachindex(width_chebpoints)
-    width = width_chebpoints[j]
-        PythonCall.GIL.@lock for (i, freq) in enumerate(freq_chebpoints)
-            λ_µm = convert_freq_unitless_to_λ_µm(freq, php)
-            pillar_ϵ = get_pillar_ϵ(λ_µm)
-            substrate_ϵ = get_substrate_ϵ(λ_µm)
-            transmission = get_transmission(freq, width, pillar_height, pillar_ϵ, unit_cell_length, substrate_ϵ, php.nG)
-            transmission_matrix[i, j] = transmission
-        end
+    Threads.@threads for idx in CartesianIndices((eachindex(freq_chebpoints), eachindex(width_chebpoints)))
+        i, j = Tuple(idx)
+        @info "freq: $(i) / $(length(freq_chebpoints)), width: $(j) / $(length(width_chebpoints))"
+        freq = freq_chebpoints[i]
+        width = width_chebpoints[j]
+        λ_µm = convert_freq_unitless_to_λ_µm(freq, php)
+        pillar_ϵ = get_pillar_ϵ(λ_µm)
+        substrate_ϵ = get_substrate_ϵ(λ_µm)
+        transmission = readchomp(`python3 python_scripts/get_transmission.py $freq $width $pillar_height $pillar_ϵ $unit_cell_length $substrate_ϵ $(php.nG)`)
+        transmission_matrix[i, j] = parse(ComplexF64, transmission)
     end
     transmission_matrix
 end
