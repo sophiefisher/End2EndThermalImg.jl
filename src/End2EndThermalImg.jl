b253661712f2fb441a07ec60fa20e88ee576b71d
Sophie <fisher.soph@gmail.com>
2025-04-01 13:37:17 -0400

    trying pythoncall again

--- src/End2EndThermalImg.jl
+++ src/End2EndThermalImg.jl
@@ -8,7 +8,11 @@ module End2EndThermalImg
     # surrogate.jl
     export get_transmission
     export compute_and_save_surrogate_transmission_matrix
-    
+
+    # python modules
+    export grcwa
+    export numpy
+
     using CSV
     using DataFrames
     using Interpolations
@@ -25,6 +29,13 @@ module End2EndThermalImg
     const c = 299792458
     const ħ = 6.62607015e-34
     const kB = 1.380649e-23
+    const grcwa = Ref{Py}()
+    const numpy = Ref{Py}()
+    
+    function __init__()
+        grcwa[] = pyimport("grcwa")
+        numpy[] = pyimport("numpy")
+    end
 
     include("prepare.jl")
     include("forward.jl")
