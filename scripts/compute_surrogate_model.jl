0316a3055e0535cd72a86a472fdee3c023883777
Sophie <fisher.soph@gmail.com>
2025-04-02 13:51:21 -0400

    tweaks to test

--- scripts/compute_surrogate_model.jl
+++ scripts/compute_surrogate_model.jl
@@ -25,11 +25,25 @@ End2EndThermalImg.plot_surrogate_models(php)
 
 using Distributed
 @everywhere using PythonCall
+@everywhere using End2EndThermalImg
+
+php = PhysicsHyperParams(
+    λlb_μm = 8.0, 
+    λub_μm = 12.0, 
+    freq_order = 5, 
+    focal_length_μm = 20000.0,
+    num_unit_cells = 2048,
+    unit_cell_length_μm = 4.0,
+    pillar_width_lb_μm = 1.8,
+    pillar_width_ub_μm = 2.7,
+    pillar_width_order = 5,
+    pillar_height_μm = 10.0,
+    pillar_material = "Si_no_absorption",
+    substrate_height_μm = 300.0,
+    substrate_material = "Si_no_absorption",
+    nG = 1000
+)
 
 @time pmap(1:4) do i
-    __main__, time = pyimport("__main__", "time")
-    time.sleep(4 - i)
-    __main__.foo = myid()
-    time.sleep(i)
-    i => __main__.foo
+    freq_chebpoints = End2EndThermalImg.get_freq_chebpoints(php)
 end
