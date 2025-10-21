2de0fa184e9ebdb36388e198e0985cdfc32b6c78
Sophie Fisher <sophiefisher@Sophies-MacBook-Pro-2.local>
2025-10-21 17:39:12 -0400

    new forward model functions to add noise to image and reconstruct ground truth

--- src/forward.jl
+++ src/forward.jl
@@ -287,3 +287,97 @@ function make_image_from_3D_oop(object, incidents, geoms, n2f_kernels, php::Phys
     # TODO: need to add noise # actually I think noise should be added in a separate function
     image
 end
+
+generate_noise!(noise_buf) = randn!(noise_buf)
+generate_noise(imghp) = randn((imghp.imgN, imghp.imgN))
+
+# generates noise inside function
+function make_noisy_image_from_3D!(
+    image_buf, noise_buf,
+    object, fftPSFs, weights,
+    php::PhysicsHyperParams, imghp::ImagingHyperParams)
+
+    make_image_from_3D!(image_buf, object, fftPSFs, weights, php, imghp)
+    noise_scale = mean(image_buf) * imghp.noise_level
+    generate_noise!(noise_buf)
+    @. image_buf += noise_scale * noise_buf
+    return image_buf
+end
+
+# pass noise to function
+function make_noisy_image_from_3D(
+    object, fftPSFs, weights, noise,
+    php::PhysicsHyperParams, imghp::ImagingHyperParams)
+
+    image = make_image_from_3D(object, fftPSFs, weights, php, imghp)
+    noise_scale = mean(image) * imghp.noise_level
+    noisy_image = image .+ noise_scale .* noise
+    return noisy_image
+end
+
+function reconstruction_objective(object_flat, noisy_image, fftPSFs, weights, α, β, T_background, php, imghp)
+    object = unflatten_object(object_flat)
+    τmap = object.Tmap
+    ζmap = object.zmap
+    image = make_image_from_3D(object, fftPSFs, weights, php, imghp)
+    error_image = sum((image .- noisy_image).^2)
+    regularization_τ = α * sum((τmap .- T_background).^2)
+    regularization_ζ = β * sum(ζmap.^2)
+    error_image + regularization_τ + regularization_ζ
+end
+
+# TODO: might want to set xtol_rel, maxeval, as rechp parameters
+# TODO: change NLopt optimization to in-place
+function reconstruct_Tmap_and_zmap(noisy_image, fftPSFs, weights, α, β, jhp::JobHyperParams; xtol_rel = 1e-8, maxeval = 5000, iteration_print = 50, verbose = false)
+    verbose && @info "Starting object reconstruction"
+    @unpack php, imghp, rechp = jhp
+    @unpack T_background = rechp
+    
+    objective_history = Float64[]
+    object_init = initialize_object(imghp, rechp)
+    object_init_flat = flatten_object(object_init)
+    
+    opt = Opt(:LD_LBFGS, 2 * imghp.objN^2) # TODO: check algorithm choice
+    lower_bounds!(opt, [fill(eps(), imghp.objN^2); fill(imghp.PSF_zlb, imghp.objN^2)])
+    upper_bounds!(opt, [fill(Inf, imghp.objN^2); fill(imghp.PSF_zub, imghp.objN^2)])
+    objective_lambda = object_flat -> reconstruction_objective(object_flat, noisy_image, fftPSFs, weights, α, β, T_background, php, imghp)
+    objective_wrapped_lambda = (x, grad) -> nlopt_wrap_objective_autodiff(x, grad, objective_lambda, objective_history; iteration_print = iteration_print, verbose = verbose)
+    max_objective!(opt, objective_wrapped_lambda)
+    xtol_rel!(opt, xtol_rel)
+    maxeval!(opt, maxeval)
+
+    (objective_opt, object_opt_flat, return_value) = NLopt.optimize!(opt, object_init_flat)
+    verbose && @info "Done object reconsstruction"
+    verbose && @info "Optimization results" objective_opt return_value
+    object_opt = unflatten_object(object_opt_flat)
+    (; objective_opt, object_opt, return_value, objective_history)
+end
+
+function finite_difference_gradient_central(f, x; ε = 1e-6)
+    n = length(x)
+    grad_fd = similar(x)
+    for i in 1:n
+        x_plus = copy(x);  x_plus[i] += ε
+        x_minus = copy(x); x_minus[i] -= ε
+        grad_fd[i] = (f(x_plus) - f(x_minus)) / (2ε)
+    end
+    return grad_fd
+end
+
+function test_reconstruction_gradients(noisy_image, fftPSFs, weights, α, β, jhp; ε = 1e-6)
+    @unpack php, imghp, rechp = jhp
+    @unpack T_background = rechp
+    
+    object_init = initialize_object(imghp, rechp)
+    object_init_flat = flatten_object(object_init)
+
+    objective_lambda = object_flat -> reconstruction_objective(object_flat, noisy_image, fftPSFs, weights, α, β, T_background, php, imghp)
+
+    grad_autodiff = Zygote.gradient(x -> objective_lambda(x), object_init_flat)[1]
+    grad_fd = finite_difference_gradient_central(objective_lambda, object_init_flat; ε = ε)
+
+    # compare
+    diff_norm = norm(grad_autodiff - grad_fd) / (norm(grad_fd))
+
+    @info "Relative gradient error for ε=$(ε): $diff_norm"
+end
