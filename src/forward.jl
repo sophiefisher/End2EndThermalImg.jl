4c0668c1b805ee528743be652d7df8e2050b6277
Sophie Fisher <sophiefisher@Sophies-MacBook-Pro-2.local>
2026-02-23 14:39:01 -0500

    fix bug related to finding the depths to interpolate to

--- src/forward.jl
+++ src/forward.jl
@@ -281,7 +281,6 @@ function make_image_from_3D!(image_buf, object, fftPSFs, weights, php::PhysicsHy
             PSF_zupper_idx = PSF_zlower_idx + 1
         end
         PSF_zlower = PSF_zcoords[PSF_zlower_idx]
-        PSF_zupper_idx = PSF_zlower_idx + 1
         PSF_zupper = PSF_zcoords[PSF_zupper_idx]
 
         C_zlower = δ_Δz(PSF_zlower - z)
