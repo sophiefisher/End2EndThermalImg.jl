aef1e801915a88d38412de768af594ec533a2f3a
Sophie Fisher <sophiefisher@Sophies-MacBook-Pro-2.local>
2026-02-25 12:00:46 -0500

    thread get_fftPSFs function

--- src/forward.jl
+++ src/forward.jl
@@ -221,7 +221,8 @@ function get_fftPSFs(freqs, incidents, surrogates, geoms, n2f_kernels, plans_n2f
     PSFs = Matrix{Matrix{ComplexF64}}(undef, nF, PSF_zlen)
     inds = CartesianIndices((1:nF, 1:PSF_zlen))
 
-    Threads.@threads for idx in eachindex(inds)
+    @threads for idx in eachindex(inds)
+        tid = threadid()
         I = inds[idx]
         iF, iZ = I[1], I[2]
 
