042e9af6e30d3930d7cf6b4488cd146b58ea1bfa
Sophie Fisher <sophiefisher@dhcp-10-29-175-1.dyn.mit.edu>
2026-07-08 17:15:00 -0400

    tweak script settings

--- cluster_scripts/run_test_surrogate_memory.sh
+++ cluster_scripts/run_test_surrogate_memory.sh
@@ -11,6 +11,6 @@ module load anaconda/Python-ML-2025a
 module load julia/1.11.3
 
 
-PROJECT_DIR="~/End2EndThermalImg.jl/Project.toml"
+PROJECT_DIR="$HOME/End2EndThermalImg.jl"
 export JULIA_CONDAPKG_BACKEND="Null"
-julia --project=${PROJECT_DIR} -p ${SLURM_NTASKS} scripts/test_surrogate_memory.jl
+/usr/bin/time -v julia --project=${PROJECT_DIR} scripts/test_surrogate_memory.jl
