9d08631810913dc166a873ba793821e333c4babf
Sophie Fisher <sefisher@mit.edu>
2026-07-10 18:19:50 -0400

    tweaks to script

--- cluster_scripts/run_test_surrogate_memory.sh
+++ cluster_scripts/run_test_surrogate_memory.sh
@@ -2,6 +2,7 @@
 
 #SBATCH --job-name=test_surrogate_memory
 #SBATCH --output=logs/log-%j-%x.out
+#SBATCH --cpus-per-task=8
 
 #initialize module command
 source /etc/profile
@@ -13,4 +14,7 @@ module load julia/1.11.3
 
 PROJECT_DIR="$HOME/End2EndThermalImg.jl"
 export JULIA_CONDAPKG_BACKEND="Null"
+export OPENBLAS_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1      # add this too — some numpy builds use MKL rather than OpenBLAS
 /usr/bin/time -v julia --project=${PROJECT_DIR} scripts/test_surrogate_memory.jl
